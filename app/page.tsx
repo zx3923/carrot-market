6ea4b65c35cd5f1bd15aa55f3c1180eedd32c4e5
zx3923 <seop3923@gmail.com>
2024-08-20 17:18:06 +0900

    components style

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,7 @@ export default function Home() {
         <h2 className="text-sm">지금 내 동네를 선택하고 시작해보세요!</h2>
       </div>
       <div className="flex flex-col items-center gap-3 w-full">
-        <Link
-          href="/create-account"
-          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
-        >
+        <Link href="/create-account" className="primary-btn text-lg py-2.5">
           시작하기
         </Link>
         <div className="flex gap-2">
