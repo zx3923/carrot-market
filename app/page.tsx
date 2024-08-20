086eeec0e53a2f7650f4f53e5ce88a032d0bffc1
zx3923 <seop3923@gmail.com>
2024-08-20 16:55:29 +0900

    홈 페이지

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
+      <div className="my-auto flex flex-col items-center gap-3 *:font-medium">
+        <span className="text-9xl">🥕</span>
+        <h1 className="text-3xl ">당신 근처의 당근</h1>
+        <h2 className="text-sm">동네라서 가능한 모든 것</h2>
+        <h2 className="text-sm">지금 내 동네를 선택하고 시작해보세요!</h2>
+      </div>
+      <div className="flex flex-col items-center gap-3 w-full">
+        <Link
+          href="/create-account"
+          className="w-full bg-orange-500 text-white text-lg font-medium py-2.5 rounded-md text-center hover:bg-orange-400 transition-colors"
+        >
+          시작하기
+        </Link>
+        <div className="flex gap-2">
+          <span>이미 계정이 있나요?</span>
+          <Link href="/login" className="hover:underline">
+            로그인
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
