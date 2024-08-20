db73c2e9ec5426b9e8c4c7cd9ff6f2347a55a3cc
zx3923 <seop3923@gmail.com>
2024-08-20 21:48:58 +0900

    renamed

--- components/input.tsx
+++ components/input.tsx
@@ -1,23 +1,19 @@
-interface FormInputProps {
-  type: string;
-  placeholder: string;
-  required: boolean;
-  errors: string[];
+import { InputHTMLAttributes } from "react";
+interface InputProps {
+  name: string;
+  errors?: string[];
 }
-
-export default function FormInput({
-  type,
-  placeholder,
-  required,
-  errors,
-}: FormInputProps) {
+export default function Input({
+  name,
+  errors = [],
+  ...rest
+}: InputProps & InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-2">
       <input
+        name={name}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400 pl-2"
-        type={type}
-        placeholder={placeholder}
-        required={required}
+        {...rest}
       />
       {errors.map((error, index) => (
         <span key={index} className="text-red-500 font-medium">
