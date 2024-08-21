73512eaba79474113dbafb72606f5550fc2c9c9d
zx3923 <seop3923@gmail.com>
2024-08-21 20:27:27 +0900

    login page

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -4,15 +4,47 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { successLogin } from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  // if(user){
+  //   return true
+  // } else {
+  //   return false
+  // }
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
-  password: z
-    .string({
-      required_error: "Password is required",
+  email: z
+    .string()
+    .email({
+      message: "올바른 이메일 주소를 입력해주세요.",
     })
-    .min(PASSWORD_MIN_LENGTH)
+    .toLowerCase()
+    .refine((email) => {
+      const isValidEmail = z.string().email().safeParse(email).success;
+      if (!isValidEmail) {
+        return true; // 이메일 형식이 잘못된 경우 다음 검증을 스킵.
+      }
+      // 이메일이 유효한 형식일 경우에만 존재유무 검증 실행
+      return checkEmailExists(email);
+    }, "존재하지 않는 이메일 입니다"),
+  password: z
+    .string()
+    .min(PASSWORD_MIN_LENGTH, "비밀번호는 6자 이상입니다")
     .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
 });
 
@@ -21,11 +53,36 @@ export async function logIn(prevState: any, formData: FormData) {
     email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
   if (!result.success) {
-    console.log(result.error.flatten());
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const user = await db.user.findUnique({
+      where: {
+        email: result.data.email,
+      },
+      select: {
+        id: true,
+        password: true,
+      },
+    });
+    const ok = await bcrypt.compare(
+      result.data.password,
+      user!.password ?? "XXXX"
+    );
+    if (ok) {
+      await successLogin(user!.id);
+      // const session = await getSession();
+      // session.id = user!.id;
+      // await session.save();
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          password: ["잘못된 비밀번호 입니다"],
+          email: [],
+        },
+      };
+    }
   }
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import FormButton from "@/components/button";
-import FormInput from "@/components/input";
+import Button from "@/components/button";
+import Input from "@/components/input";
 import SocialLogin from "@/components/social-login";
 import { useFormState } from "react-dom";
 import { logIn } from "./actions";
@@ -16,22 +16,22 @@ export default function LogIn() {
         <h2 className="text-sm">이메일과 비밀번호로 로그인 하세요.</h2>
       </div>
       <form action={action} className="flex flex-col gap-3">
-        <FormInput
+        <Input
           name="email"
           type="email"
           placeholder="이메일"
-          required
+          // required
           errors={state?.fieldErrors.email}
         />
-        <FormInput
+        <Input
           name="password"
           type="password"
           placeholder="비밀번호"
-          required
+          // required
           minLength={PASSWORD_MIN_LENGTH}
           errors={state?.fieldErrors.password}
         />
-        <FormButton text="로그인" />
+        <Button text="로그인" />
       </form>
       <SocialLogin />
     </div>
