8dfcc0d2b71443f1b66a4f6090ccf91aca97672c
zx3923 <seop3923@gmail.com>
2024-08-24 20:06:57 +0900

    add url 'public'

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -48,7 +48,7 @@ export default async function ProductDetail({
   return (
     <div>
       <div className="relative aspect-square">
-        <Image fill src={product.photo} alt={product.title} />
+        <Image fill src={`${product.photo}/public`} alt={product.title} />
       </div>
       <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
         <div className="size-10 overflow-hidden rounded-full">
