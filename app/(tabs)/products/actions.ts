eaf5f57de1435108c4fcc96ff09165a86c916eab
zx3923 <seop3923@gmail.com>
2024-08-24 20:06:16 +0900

    1 -> 5

--- app/(tabs)/products/actions.ts
+++ app/(tabs)/products/actions.ts
@@ -11,8 +11,8 @@ export async function getMoreProducts(page: number) {
       photo: true,
       id: true,
     },
-    skip: page * 1,
-    take: 1,
+    skip: page * 5,
+    take: 5,
     orderBy: {
       created_at: "desc",
     },
