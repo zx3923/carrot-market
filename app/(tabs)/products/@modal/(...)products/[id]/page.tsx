b911ad54883aa175722e2c2b11280a6c4d518339
zx3923 <seop3923@gmail.com>
2024-08-26 12:29:42 +0900

    modal style

--- app/(tabs)/products/@modal/(...)products/[id]/page.tsx
+++ app/(tabs)/products/@modal/(...)products/[id]/page.tsx
@@ -1,24 +1,79 @@
-"use client";
+import CloseBtn from "@/components/close-btn";
+import db from "@/lib/db";
+import { formatToWon } from "@/lib/utils";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import { UserIcon } from "@heroicons/react/24/solid";
 
-import { PhotoIcon, XMarkIcon } from "@heroicons/react/24/solid";
-import { useRouter } from "next/navigation";
+async function getProduct(id: number) {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          user_name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  return product;
+}
 
-export default function Modal({ params }: { params: { id: string } }) {
-  const router = useRouter();
-  const onCloseClick = () => {
-    router.back();
-  };
+export default async function Modal({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
+  const product = await getProduct(id);
+
+  if (!product) {
+    return notFound();
+  }
+
   return (
-    <div className="absolute w-full h-full z-50 flex items-center justify-center bg-black bg-opacity-60 left-0 top-0">
-      <button
-        onClick={onCloseClick}
-        className="absolute right-5 top-5 text-neutral-200"
-      >
-        <XMarkIcon className="size-10" />
-      </button>
-      <div className="max-w-screen-sm h-1/2  flex justify-center w-full">
-        <div className="aspect-square  bg-neutral-700 text-neutral-200  rounded-md flex justify-center items-center">
-          <PhotoIcon className="h-28" />
+    <div className="max-w-screen-sm flex justify-center items-center bg-black bg-opacity-60">
+      <div className="relative bg-white rounded-lg shadow-lg max-w-4xl w-full h-auto p-6">
+        <CloseBtn />
+        <h1 className="text-2xl font-bold text-gray-900 mb-4 border-b pb-2">
+          상품 더보기
+        </h1>
+        <div className="flex flex-col md:flex-row gap-4">
+          <div className="w-full md:w-1/2 h-60 md:h-80 relative rounded-lg overflow-hidden bg-gray-100">
+            <Image
+              fill
+              src={`${product.photo}/public`}
+              alt={product.title}
+              className="object-cover"
+            />
+          </div>
+          <div className="flex flex-col justify-between">
+            <div className="flex flex-col gap-2 mb-4">
+              <h2 className="text-xl font-semibold text-gray-900">
+                {product.title}
+              </h2>
+              <p className="text-sm font-medium text-gray-700">
+                {formatToWon(product.price)}원
+              </p>
+              <p className="text-neutral-500">{product.description}</p>
+            </div>
+            <div className="flex items-center gap-4 mt-2">
+              <div className="w-10 h-10 relative rounded-full overflow-hidden bg-gray-200">
+                {product.user.avatar ? (
+                  <Image
+                    fill
+                    src={`${product.user.avatar}/avatar`}
+                    alt={product.user.user_name}
+                    className="object-cover"
+                  />
+                ) : (
+                  <UserIcon />
+                )}
+              </div>
+              <h3 className="text-lg font-medium text-gray-800">
+                {product.user.user_name}
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </div>
