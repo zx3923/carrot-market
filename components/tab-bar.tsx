d97d0e303acf59aeef0dd1bb67f9ec0037bcd57d
zx3923 <seop3923@gmail.com>
2024-08-27 16:13:46 +0900

    deploy

--- components/tab-bar.tsx
+++ components/tab-bar.tsx
@@ -11,7 +11,6 @@ import {
   HomeIcon as OutlineHomeIcon,
   NewspaperIcon as OutlineNewspaperIcon,
   ChatBubbleOvalLeftEllipsisIcon as OutlineChatIcon,
-  VideoCameraIcon as OutlineVideoCameraIcon,
   UserIcon as OutlineUserIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
@@ -20,7 +19,7 @@ import { usePathname } from "next/navigation";
 export default function TabBar() {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 w-full mx-auto max-w-screen-sm grid grid-cols-5 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
+    <div className="fixed bottom-0 w-full mx-auto max-w-screen-sm grid grid-cols-4 border-neutral-600 border-t px-5 py-3 *:text-white bg-neutral-800">
       <Link href="/products" className="flex flex-col items-center gap-px">
         {pathname === "/products" ? (
           <SolidHomeIcon className="w-7 h-7" />
@@ -44,14 +43,6 @@ export default function TabBar() {
           <OutlineChatIcon className="w-7 h-7" />
         )}
         <span>채팅</span>
-      </Link>
-      <Link href="/live" className="flex flex-col items-center gap-px">
-        {pathname === "/live" ? (
-          <SolidVideoCameraIcon className="w-7 h-7" />
-        ) : (
-          <OutlineVideoCameraIcon className="w-7 h-7" />
-        )}
-        <span>쇼핑</span>
       </Link>
       <Link href="/profile" className="flex flex-col items-center gap-px">
         {pathname === "/profile" ? (
